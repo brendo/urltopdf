0f121d6acca7899dee48f5249e290707ec9db877
Andrew Davis <andrew.davis@thinkorchard.com>
2014-04-02 10:49:28 +0100

    preferences stripped back

--- content/content.preferences.php
+++ content/content.preferences.php
@@ -5,40 +5,24 @@
 	require_once(TOOLKIT . '/class.eventmanager.php');
 	require_once(TOOLKIT . '/class.pagemanager.php');
 
-	class contentExtensionURLtoPdfPreferences extends AdministrationPage 
-	{
+	class contentExtensionURLtoPdfPreferences extends AdministrationPage{
 		protected $driver;
-
-		public function __viewIndex($context) 
-		{
-						/*                                                                         */
-						
-					
-						
-						
-						
-						
-						/*                                                                         */
-						if(file_exists(WORKSPACE.'/assets/css/pdf'))
-						{
+		public function __viewIndex($context){
+						if(file_exists(WORKSPACE.'/assets/css/pdf')){
 							$path = scandir(WORKSPACE.'/assets/css/pdf');
-						}else
-						{
+						}else{
 							$path = scandir(EXTENSIONS.'/urltopdf/assets/css');
 						}
 						$arraydiffs = array('.','..');
 						$arrs = array_diff($path,$arraydiffs);
-						foreach($arrs as $dirs => $key)
-						{
+						foreach($arrs as $dirs => $key){
 									$v = explode('.',$key);
 									array_pop($v);
-									if(array_count_values($v) > 1)
-									{
+									if(array_count_values($v) > 1){
 											$a = implode('.',$v);
 											$imp[] = array(URL.'/workspace/assets/css/pdf/'.$key,'0',$a);
 									}
-									else
-									{
+									else{
 											$a = implode($v);
 											$imp[] = array(URL.'/workspace/assets/css/pdf/'.$key,'0',$a);
 									}
@@ -77,21 +61,14 @@
 										$documentationlbl = new XMLElement('label');
 										$documentationlbl->setValue('<strong>DOCUMENTATION:</strong><br/>');
 										$documentation = new XMLElement('code');
-										$text = 'In Order to Use the Extension Correctly append the page type as download and add a URL GET Param as  in example "http://current-url/?display=pdf" or  "http://current-url/?display=doc"these are to view it as a format. to create a link for the user to download a file it must be in this format "http://current-url/?display=doc&download=docx" , "http://current-url/?display=pdf&download=pdf" ';
+										$text = 'In Order to Use the Extension Correctly append the page type as download and add a URL GET Param as  in example "http://current-url/?display=pdf".';
 										$documentation->setValue($text);
 										$documentationlbl->appendChild($documentation);
 										$sellabel = new XMLElement('label');
 										$sellabel->setAttribute('class','column');
 										$sellabel->setValue('CSS file');
 										$sellabel->appendChild(Widget::Select('template-css',$imp));
-										$group->appendChild($sellabel);
-										$col2 = new XMLElement('div');
-										$col2->setAttribute('class','column');
-										$templabel = new XMLElement('label');
-										$templabel->setValue('Template File');																				
-										$templabel->appendChild(Widget::Select('template-file',$templateoptions));
-										$col2->appendChild($templabel);
-										$group->appendChild($col2);
+										$group->appendChild($sellabel);										
 										$container->appendChild($group);
 										$container->appendChild($documentationlbl);
 										$this->Form->appendChild($container);
