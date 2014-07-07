c3aae860f945205adb1b42c5baff023919e9cca0
Andrew Minton <me@andrewminton.co.uk>
2014-07-07 17:02:05 +0100

    Switch to MPDF library
    Change from TCPDF to mPDF library for PDF output.

--- extension.driver.php
+++ extension.driver.php
@@ -2,20 +2,6 @@
 
 	Class Extension_URLtoPDF extends Extension {
 
-		public function about() {
-			return array(
-				'name' => 'URL to PDF',
-				'version' => '0.1',
-				'release-date' => '2011-07-13',
-				'author' => array(
-					'name' => 'Brendan Abbott',
-					'website' => 'http://www.bloodbone.ws',
-					'email' => 'brendan@bloodbone.ws'
-				),
-				'description' => 'Uses the TCPDF library to take your HTML page and output it as a PDF'
-			);
-		}
-
 	/*-------------------------------------------------------------------------
 		Definition:
 	-------------------------------------------------------------------------*/
@@ -38,7 +24,7 @@
 		 */
 		public function generatePDFfromURL(array &$context = null) {
 			$page_data = Frontend::Page()->pageData();
-			if(EXTENSIONS.'/urltofiletype/lib/MPDF56/tmp' == false){
+			if(EXTENSIONS.'/urltopdf/lib/MPDF56/tmp' == false){
 				$director_created = mkdir(EXTENSIONS.'/urltopdf/lib/MPDF56/tmp');
 			}
 			if(!isset($page_data['type']) || !is_array($page_data['type']) || empty($page_data['type'])) return;
@@ -56,10 +42,10 @@
 		}
 
 		public function generatePDF($output) {
-			require_once(EXTENSIONS . '/urltopdf/lib/MPDF56/mpdf.php');
+			
 			$params = Frontend::Page()->_param;
-			$pdf=new mPDF();
-			//$pdf = self::initPDF();
+
+			$pdf = self::initPDF();
 			
 			$path = EXTENSIONS.'/urltopdf/assets/css/default.css';
 			$pdf->SetAuthor($params['website-name']);
@@ -69,10 +55,6 @@
 			$stylesheet = file_get_contents($path);
 			$pdf->WriteHTML($stylesheet,1);
 			$pdf->writeHTML($output);
-			//$pdf->writeHTML($output, true, false, true, false, '');
-
-			// reset pointer to the last page
-			//$pdf->lastPage();
 
 			//Close and output PDF document
 			$pdf->Output(sprintf('%s - %s', $params['website-name'], $params['page-title']), 'I');
@@ -82,7 +64,7 @@
 			$params = Frontend::Page()->_param;
 
 			$dom = new DOMDocument('1.0', 'UTF-8');
-			$doc->formatOutput = true;
+			//$doc->formatOutput = true;
 			$dom->loadHTML($output);
 
 			if($dom === false) return $output;
@@ -104,23 +86,23 @@
 
 				// Send the block to the PDF generator, saving it in /TMP
 				$data = $css . $data;
-				$pdf=new mPDF();
+
+				$pdf = self::initPDF();
+
+				$pdf->SetAuthor($params['website-name']);
+				$pdf->SetTitle($params['page-title']);
 
 				// output the HTML content
 				$pdf->writeHTML($data);
-				//$pdf->writeHTML($data, true, false, true, false, '');
-
-				// reset pointer to the last page
-				//$pdf->lastPage();
 
 				// get the output of the PDF as a string and save it to a file
 				// attempt to find the filename if it's provided with @data-utp-filename
 				if(!$filename = $xpath->evaluate('string(//@data-utp-filename)')) {
 					 $filename = md5(sprintf('%s - %s', $params['website-name'], $params['page-title']));
 				}
-				$filename = TMP . '/' . Lang::createFilename($filename) . '.pdf';
+				$filename = MANIFEST . '/tmp/' . Lang::createFilename($filename) . 'pdf';
 
-				General::writeFile($filename, $pdf->Output($filename, 'S'), Symphony::Configuration()->get('write_mode', 'file'));
+				General::writeFile($filename, $pdf->Output($pdf, 'S'), Symphony::Configuration()->get('write_mode', 'file'));
 
 				// Replace the attachment node with <link rel='attachment' href='{path/to/file}' />
 				$link = $dom->createElement('link');
@@ -133,32 +115,16 @@
 			$output = $dom->saveHTML();
 		}
 		private static function initPDF() {
-			require_once(EXTENSIONS . '/urltopdf/lib/tcpdf/config/lang/eng.php');
-			require_once(EXTENSIONS . '/urltopdf/lib/tcpdf/tcpdf.php');
+			require_once(EXTENSIONS . '/urltopdf/lib/MPDF56/mpdf.php');
 
 			// create new PDF document
-			$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-			// set document information
-			$pdf->SetCreator(PDF_CREATOR);
-
-			$pdf->setPrintHeader(false);
-			$pdf->setPrintFooter(false);
-
-			// set default monospaced font
-			$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+			$pdf = new mPDF();
 
 			//set margins
 			$pdf->SetMargins(20, 20, 20);
 
 			//set auto page breaks
 			$pdf->SetAutoPageBreak(TRUE, 20);
-
-			//set image scale factor
-			$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-			//set some language-dependent strings
-			$pdf->setLanguageArray($l);
 
 			// add a page
 			$pdf->AddPage();
